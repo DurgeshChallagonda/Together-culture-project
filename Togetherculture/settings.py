4e4831761c6c611c7a3dd2203c28c71b5ee1237d
Joshua Ebuka <49676104+kidjosh001@users.noreply.github.com>
2025-03-03 13:51:53 +0000

    Created a redirect function back to my custom admin login after logging out. Also, a dummy new_admin_dashboard.html file for testing purposes only. This file will be deleted in the future.

--- Togetherculture/settings.py
+++ Togetherculture/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+LOGOUT_REDIRECT_URL = '/adminlogin.html'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
